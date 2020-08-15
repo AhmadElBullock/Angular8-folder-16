2c7c21d9971d5277ccdbf7620667e3d0f244c07b
AhmadElBullock <ahmadelbullock@gmail.com>
2020-08-15 20:41:38 +0200

    cleaning shopping-edit.comp.ts

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingService } from '../shopping.service';
 import { NgForm } from '@angular/forms';
@@ -9,8 +9,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  //@ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  //@ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
   //@Output() newIngredient = new EventEmitter<Ingredient>()
   constructor(private shoppingService: ShoppingService) { }
 
